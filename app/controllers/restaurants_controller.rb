79a72751f49fde979a0bb03930bd2ab6863aa7c8
Serena Hathi <hathi.serena@gmail.com>
2018-03-13 16:58:40 +0000

    User can add a new restaurant and view all restaurant listings. Test passes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,11 @@
 class RestaurantsController < ApplicationController
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def index
+    @restaurants = Restaurant.all
   end
 
   def new
@@ -10,9 +16,10 @@ class RestaurantsController < ApplicationController
     @restaurant.save
     redirect_to @restaurant
   end
-
+  
   private
   def restaurant_params
     params.require(:restaurant).permit(:name, :description)
   end
+
 end
