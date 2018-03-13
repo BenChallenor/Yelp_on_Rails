0947e2c7655f305be589867e995580a25a896e9b
Ben <ben.challenor@btopenworld.com>
2018-03-13 16:28:23 +0000

    Added create method

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,8 +4,15 @@ class RestaurantsController < ApplicationController
 
   def new
   end
-  
+
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to @restaurant
   end
 
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description)
+  end
 end
