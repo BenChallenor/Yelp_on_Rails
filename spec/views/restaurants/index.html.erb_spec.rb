099588aff6d67a274d3305d05c2718c7dc527295
Serena Hathi <hathi.serena@gmail.com>
2018-03-13 14:18:11 +0000

    Set up restaurant controller and route. First Capybara test passes

---
+++ spec/views/restaurants/index.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+# require 'capybara/rspec'
+
+# RSpec.describe "restaurant/index.html.erb", type: :view do
+#   visit "/"
+#   expect(page).to have_content("Restaurant listings")
+# end
+
+
+describe "Testing infrastructure" do
+  scenario "App loads and content is displayed" do
+    visit("/restaurant/index")
+    expect(page).to have_content("Restaurant listings")
+  end
+end
