c5ae0eca8fafee61f6c3526f93eb10da67de73fc
marcusfgardiner <marcusfgardiner@gmail.com>
2018-03-14 11:58:45 +0000

    Back button feature test added and passing

---
+++ spec/views/reviews/show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'user can click back to all reviews page' do
+  scenario 'viewing an individual review' do
+    visit '/reviews'
+    click_link "Add review"
+    fill_in 'review[rating]', with: 5
+    fill_in 'review[comment]', with: "yum"
+    click_button 'Save Review'
+    click_link 'Back to all reviews'
+    expect(current_path).to eq('/reviews')
+  end
+end
