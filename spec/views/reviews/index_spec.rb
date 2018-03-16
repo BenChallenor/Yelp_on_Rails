9000988791c721a73e4ca8fe55233a905a46fb33
Serena Hathi <hathi.serena@gmail.com>
2018-03-16 14:56:13 +0000

    Added validation for reviews and corresponding feature tests

--- spec/views/reviews/index_spec.rb
+++ spec/views/reviews/index_spec.rb
@@ -2,12 +2,7 @@ require 'rails_helper'
 
 feature 'user can add review and see the review on the restaurants page' do
   scenario 'User visits restaurant index page' do
-    visit("/restaurants/new")
-    fill_in "restaurant[name]", with: "Flat iron"
-    fill_in "restaurant[description]", with: "Steak house"
-    click_button "Create Restaurant"
-    click_link "Back to restaurant listings"
-    click_link "Show"
+    add_restaurant
     click_link "Add Review"
     fill_in 'review[rating]', with: 5
     fill_in 'review[comment]', with: "yum"
@@ -15,5 +10,43 @@ feature 'user can add review and see the review on the restaurants page' do
     expect(page).to have_content 'yum'
     expect(page).to have_content '5'
   end
+
+  scenario "User must complete rating field" do
+    add_restaurant
+    click_link "Add Review"
+    fill_in 'review[comment]', with: "yum food"
+    click_button 'Save Review'
+    expect(page).to have_content("Rating can't be blank")
+  end
+
+  scenario "User rating must be 5 or less" do
+    add_restaurant
+    click_link "Add Review"
+    fill_in 'review[rating]', with: 6
+    click_button 'Save Review'
+    expect(page).to have_content("Rating must be less than or equal to 5")
+  end
+
+  scenario "User must complete comment field" do
+    add_restaurant
+    click_link "Add Review"
+    fill_in 'review[rating]', with: 5
+    click_button 'Save Review'
+    expect(page).to have_content("Comment can't be blank")
+  end
+
+  scenario "User comment must be 5 characters or more" do
+    add_restaurant
+    click_link "Add Review"
+    fill_in 'review[comment]', with: "yum"
+    click_button 'Save Review'
+    expect(page).to have_content("Comment is too short (minimum is 5 characters)")
+  end
+
+
+
+  
+  
+
 
 end
